90450322d30346bee2fdf9c51e586b3af61f9fe1
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-29 23:53:00 +0200

    Create models.py

---
+++ Migrations and Django Admin/Exercise/Movie/models.py
@@ -0,0 +1,8 @@
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    release_year = models.PositiveIntegerField()
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Movie "{self.title}" by {self.director}'
