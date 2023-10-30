227668c203486bd575204e66d489125e6aabe70a
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-30 23:46:35 +0200

    Create models.py

---
+++ Data Operations in Django with Queries/Exercise/Artifact/models.py
@@ -0,0 +1,6 @@
+class Artifact(models.Model):
+    name = models.CharField(max_length=70)
+    origin = models.CharField(max_length=70)
+    age = models.PositiveIntegerField()
+    description = models.TextField()
+    is_magical = models.BooleanField(default=False)
