ed78b31ac31e250848df3de3859179a9429bd5c1
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-11-04 23:27:16 +0200

    Create caller.py

---
+++ Django Models Relations/Exercise/Music App/caller.py
@@ -0,0 +1,15 @@
+def add_song_to_artist(artist_name: str, song_title: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.add(song)
+
+
+def get_songs_by_artist(artist_name: str) -> QuerySet[Song]:
+    artist = Artist.objects.get(name=artist_name)
+    return artist.songs.all().order_by('-id')
+
+
+def remove_song_from_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    artist.songs.get(title=song_title).delete()
