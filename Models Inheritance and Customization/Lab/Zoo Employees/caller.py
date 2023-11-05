e969e7045aed03ac1833a0c71778e0bc2d335b37
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-11-05 23:55:12 +0200

    Create caller.py

---
+++ Models Inheritance and Customization/Lab/Zoo Employees/caller.py
@@ -0,0 +1,17 @@
+from main_app.models import ZooKeeper, Veterinarian, Mammal
+
+zookeeper = ZooKeeper.objects.create(first_name="Peter", last_name="Johnson", phone_number="0899524265", specialty="Mammals")
+
+mammal = Mammal.objects.get(name="Fluffy")
+
+zookeeper.managed_animals.add(mammal)
+
+veterinarian = Veterinarian.objects.create(first_name="Dr. Michael", last_name="Smith", phone_number="9876543210", license_number="VET12345")
+
+zookeeper_from_db = ZooKeeper.objects.first()
+
+print(f"{zookeeper_from_db.first_name} {zookeeper_from_db.last_name} is a ZooKeeper.")
+
+veterinarian_from_db = Veterinarian.objects.first()
+
+print(f"{veterinarian_from_db.first_name} {veterinarian_from_db.last_name} is a Veterinarian.")
